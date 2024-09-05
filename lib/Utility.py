186ed2d6147ef6d7f2e72f1b3135022497436445
Vittorio Niespolo <vittorio.niespolo@micegroup.it>
2024-09-05 20:09:03 +0200

    Under

--- lib/Utility.py
+++ lib/Utility.py
@@ -4,7 +4,11 @@ import pandas as pd
 import numpy as np  # Fundamental package for scientific computing with Python
 from datetime import date, timedelta
 from sklearn import preprocessing
-
+# Train the model
+from sklearn.linear_model import LogisticRegression
+from xgboost import XGBClassifier
+from sklearn.utils.class_weight import compute_class_weight
+import joblib
 
 def exportExcelWithTimeStamp(df, prefix, postfix):
     current_GMT = time.gmtime()
@@ -65,24 +69,92 @@ def calculate_gain_quotaMin(row, quotaMin):
 
 def calculate_gainHAD(row, model):
     # if row['predictions'+model] != 1:
-        # if row['Cluster'] == row['predictions'+model]:
-            if row['predictions'+model] == 0:
-                if (row['FTR'] == 'D'):
-                    # if(row['B365D'] >= 1.9):
-                        return row['B365D']-1
-                else:
-                    return -1
-            elif row['predictions'+model] == 1:
-                if (row['FTR'] == 'H'):
-                    # if(row['B365H'] <= 1.9):
-                        return row['B365H']-1
-                else:
-                    return -1
-            elif row['predictions'+model] == 2:
-                if (row['FTR'] == 'A'):
-                    # if(row['B365A'] >= 1.9):
-                        return row['B365A']-1
-                else:
-                        return -1
+    # if row['Cluster'] == row['predictions'+model]:
+    if row['predictions'+model] == 0:
+        if (row['FTR'] == 'D'):
+            # if(row['B365D'] >= 1.9):
+            return row['B365D']-1
+        else:
+            return -1
+    elif row['predictions'+model] == 1:
+        if (row['FTR'] == 'H'):
+            # if(row['B365H'] <= 1.9):
+            return row['B365H']-1
+        else:
+            return -1
+    elif row['predictions'+model] == 2:
+        if (row['FTR'] == 'A'):
+            # if(row['B365A'] >= 1.9):
+            return row['B365A']-1
+        else:
+            return -1
         # else:
         #     return -1
+
+
+def calculate_gain_O25(row):
+    quotaMin = 1.4
+    if (row['prediction'] == 1):
+        if row['B365>2.5'] > quotaMin:
+            if row['MatchGoal'] > 2.5:
+                return row['B365>2.5']-1
+            else:
+                return -1
+        else:
+            return 0
+    elif (row['prediction'] == 0):
+        if row['B365<2.5'] > quotaMin:
+            if row['MatchGoal'] < 2.5:
+                return row['B365<2.5']-1
+            else:
+                return -1
+        else:
+            return 0
+    else:
+        return 0
+
+
+def trainLogRegModel(x_train, y_train):
+    """
+    Train a logistic regression model.
+
+    Parameters
+    ----------
+    x_train : array-like of shape (n_samples, n_features)
+        Training data.
+
+    y_train : array-like of shape (n_samples,)
+        Target values.
+
+    Returns
+    -------
+    model : LogisticRegression
+        Trained model.
+    """
+    reg = 0.01
+
+    # train a logistic regression model on the training set
+    model = LogisticRegression(C=1/reg, multi_class='ovr',
+                               solver="liblinear", random_state=42).fit(x_train, y_train)
+    return model
+
+
+def trainGBoostModel(x_train, y_train):
+    model = XGBClassifier(
+        random_state=42,
+        min_child_weight=100,  # XGBoost non ha direttamente `min_samples_split`, ma puoi usare `min_child_weight`
+        max_depth=3,
+
+        # n_estimators=100,
+        # gamma=3,  # Puoi usare gamma per aggiungere regolarizzazione
+        # class_weight='balanced'  # XGBoost gestisce classi sbilanciate in modo diverso
+        ).fit(x_train, y_train)
+    return model
+
+def dumpModel(name, model):
+    current_GMT = time.gmtime()
+    time_stamp = calendar.timegm(current_GMT)
+    joblib.dump(model, str(time_stamp) + ' - ' + name)
+
+def loadModel(name):
+    return joblib.load(name)
