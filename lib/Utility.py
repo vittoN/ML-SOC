c1e49ced3279d1ae2efacd93423962026c869cf7
Vittorio Niespolo <vittorio.niespolo@micegroup.it>
2024-09-28 23:18:49 +0200

    avanzamenti inconcludenti

--- lib/Utility.py
+++ lib/Utility.py
@@ -1,3 +1,4 @@
+from sklearn.metrics import confusion_matrix
 import calendar
 import time
 import pandas as pd
@@ -9,6 +10,7 @@ from sklearn.linear_model import LogisticRegression
 from xgboost import XGBClassifier
 from sklearn.utils.class_weight import compute_class_weight
 import joblib
+
 
 def exportExcelWithTimeStamp(df, prefix, postfix):
     current_GMT = time.gmtime()
@@ -142,19 +144,184 @@ def trainLogRegModel(x_train, y_train):
 def trainGBoostModel(x_train, y_train):
     model = XGBClassifier(
         random_state=42,
-        min_child_weight=100,  # XGBoost non ha direttamente `min_samples_split`, ma puoi usare `min_child_weight`
+        # XGBoost non ha direttamente `min_samples_split`, ma puoi usare `min_child_weight`
+        min_child_weight=100,
         max_depth=3,
 
         # n_estimators=100,
         # gamma=3,  # Puoi usare gamma per aggiungere regolarizzazione
         # class_weight='balanced'  # XGBoost gestisce classi sbilanciate in modo diverso
-        ).fit(x_train, y_train)
+    ).fit(x_train, y_train)
     return model
 
-def dumpModel(name, model):
+
+def dumpModelWithTimeStamp(name, model):
     current_GMT = time.gmtime()
     time_stamp = calendar.timegm(current_GMT)
     joblib.dump(model, str(time_stamp) + ' - ' + name)
 
+
+def dumpModel(name, model):
+    joblib.dump(model, name)
+
+
 def loadModel(name):
     return joblib.load(name)
+
+
+esoticData = None
+data = None
+secLeaguesData = None
+fullData = None
+firstLeagueData = None
+
+
+def getSecLeaguesData():
+    global secLeaguesData
+    if (secLeaguesData is None):
+        secLeaguesData = pd.read_excel('../data/mergedSecleagues.xlsx')
+    secLeaguesData['GoalCumulativeSum'] = secLeaguesData['HomeGoalsCumulative'] + \
+        secLeaguesData['AwayGoalsCumulative']
+    secLeaguesData['GoalCumulativeSumPrev'] = secLeaguesData['GoalCumulativeSum'].shift(
+        1)
+    secLeaguesData['FormRatio'] = secLeaguesData['HomeForm'] / \
+        secLeaguesData['AwayForm']
+    secLeaguesData['RecentFormRatio'] = secLeaguesData['HomeRecentHomeForm'] / \
+        secLeaguesData['AwayRecentAwayForm']
+    secLeaguesData['UltimoScontroDiretto'] = secLeaguesData['UltimoScontroDiretto'].replace(
+        {'H': 1, 'A': 2, 'D': 0}).infer_objects(copy=False)
+
+    secLeaguesData['isOver'] = np.where(
+        secLeaguesData['MatchGoal'] > 2.5, 1, 0)
+    secLeaguesData.replace([np.inf, -np.inf], np.nan, inplace=True)
+    return secLeaguesData
+
+
+def _processData(data, filepath):
+    """
+    Metodo generale per elaborare i dati.
+    """
+    if data is None:
+        data = pd.read_excel(filepath)
+    data['GoalCumulativeSum'] = data['HomeGoalsCumulative'] + data['AwayGoalsCumulative']
+    data['GoalCumulativeSumPrev'] = data['GoalCumulativeSum'].shift(1)
+    data['FormRatio'] = data['HomeForm'] / data['AwayForm']
+    data['FormDiff'] = abs(data['HomeForm'] - data['AwayForm'])
+    data['RecentFormRatio'] = data['HomeRecentHomeForm'] / data['AwayRecentAwayForm']
+    data['RecentFormDiff'] = data['HomeRecentHomeForm'] - data['AwayRecentAwayForm']
+    data['UltimoScontroDiretto'] = data['UltimoScontroDiretto'].replace(
+        {'H': 1, 'A': 2, 'D': 0}).infer_objects(copy=False)
+    data['EloDiff'] = abs(data['elo_home'] - data['elo_away'])
+    data['isOver'] = np.where(data['MatchGoal'] > 2.5, 1, 0)
+    data.replace([np.inf, -np.inf], np.nan, inplace=True)
+    return data
+
+
+fullData = None
+
+
+def getFullData():
+    """
+    Metodo specifico per elaborare i dati Full.
+    """
+    global fullData
+    fullData = _processData(fullData, '../data/mergedFinalFull.xlsx')
+    return fullData
+
+
+esoticData = None
+
+
+def getEsoticData():
+    """
+    Metodo specifico per elaborare i dati Esotic.
+    """
+    global esoticData
+    esoticData = _processData(esoticData, '../data/mergedEsotic.xlsx')
+    return esoticData
+
+
+def getData():
+    global data
+    if (data is None):
+        data = pd.read_excel('../data/mergedDataFull2.xlsx')
+    data['GoalCumulativeSum'] = data['HomeGoalsCumulative'] + \
+        data['AwayGoalsCumulative']
+    data['GoalCumulativeSumPrev'] = data['GoalCumulativeSum'].shift(1)
+    data['FormRatio'] = data['HomeForm']/data['AwayForm']
+    data['RecentFormRatio'] = data['HomeRecentHomeForm'] / \
+        data['AwayRecentAwayForm']
+    data['UltimoScontroDiretto'] = data['UltimoScontroDiretto'].replace(
+        {'H': 1, 'A': 2, 'D': 0}).infer_objects(copy=False)
+
+    data['feat1'] = abs((data['HomeEwmaGoalsScored'] + data['HomeEwmaGoalsConceded']) /
+                        (data['AwayEwmaGoalsScored'] + data['AwayEwmaGoalsConceded']))
+    data['feat2'] = abs((data['HomeGoalsCumulative'] + data['AwayGoalsCumulative']) / (
+        data['HomeGoalsConcededCumulative'] + data['AwayGoalsConcededCumulative']))
+
+    data['feat2'].replace([np.inf, -np.inf], np.nan, inplace=True)
+    data['feat1'].replace([np.inf, -np.inf], np.nan, inplace=True)
+
+    data['isOver'] = np.where(data['MatchGoal'] > 2.5, 1, 0)
+    data.replace([np.inf, -np.inf], np.nan, inplace=True)
+    return data
+
+
+def class_accuracy(y_true, y_pred):
+    """
+    Calcola l'accuratezza per ogni classe in un modello di classificazione binaria.
+
+    Args:
+      y_true: I veri valori delle etichette.
+      y_pred: I valori delle etichette predetti.
+
+    Returns:
+      Un dizionario contenente l'accuratezza per ogni classe.
+    """
+
+    cm = confusion_matrix(y_true, y_pred)
+
+    # Calcola l'accuratezza per la classe 0
+    accuracy_class_0 = cm[0, 0] / (cm[0, 0] + cm[1, 0])
+
+    # Calcola l'accuratezza per la classe 1
+    accuracy_class_1 = cm[1, 1] / (cm[0, 1] + cm[1, 1])
+
+    return {
+        "Under": accuracy_class_0,
+        "Over": accuracy_class_1
+    }
+
+
+train_over1Data = None
+test_over1Data = None
+
+
+def getTrainOver1Data():
+    global train_over1Data
+    if (train_over1Data is None):
+        train_over1Data = pd.read_excel(
+            '../Dataframe/Train Over Step1.xlsx')
+
+    return train_over1Data
+
+
+def getTestOver1Data():
+    global test_over1Data
+    if (test_over1Data is None):
+        test_over1Data = pd.read_excel(
+            '../Dataframe/Test Over Step1.xlsx')
+
+    return test_over1Data
+
+
+altriData = None
+
+
+def getAltriData():
+    """
+    Metodo specifico per elaborare i dati Altri.
+    """
+    global altriData
+    altriData = _processData(altriData, '../data/mergedAltriDati.xlsx')
+    return altriData
